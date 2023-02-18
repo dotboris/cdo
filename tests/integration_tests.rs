95d04abb143d8118b72896a3ab642f5269138039
Boris Bera <bera.boris@gmail.com>
2023-02-18 15:58:49 -0500

    Test pwd and command path using fs::canonicalize
    OSX is fun. `/tmp/` is really in `/private/.../tmp`

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -32,8 +32,14 @@ fn test_full_run() -> Result<()> {
     fs::set_permissions(&command_path, Permissions::from_mode(0o755))?;
 
     let mut expected_stdout = String::new();
-    expected_stdout.push_str(&format!("running command {}\n", &command_path.display()));
-    expected_stdout.push_str(&format!("running in {}\n", &run_dir.display()));
+    expected_stdout.push_str(&format!(
+        "running command {}\n",
+        fs::canonicalize(&command_path)?.display()
+    ));
+    expected_stdout.push_str(&format!(
+        "running in {}\n",
+        fs::canonicalize(&run_dir)?.display()
+    ));
     expected_stdout.push_str("args are foo bar baz\n");
 
     Command::cargo_bin("cdo")?
@@ -79,7 +85,7 @@ fn test_pwd_command() -> Result<()> {
         .arg("pwd")
         .assert()
         .success()
-        .stdout(format!("{}\n", &run_dir.display()));
+        .stdout(format!("{}\n", fs::canonicalize(&run_dir)?.display()));
 
     Ok(())
 }
